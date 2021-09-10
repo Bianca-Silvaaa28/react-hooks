5b85cd27546f83e14e5959a656cc643b0a8ac42c
Bianca-Silvaaa28 <silvabianca147@gmail.com>
2021-09-10 22:18:48 +0000

    aula06 - hooks

--- hooks/src/components/layout/Menu.jsx
+++ hooks/src/components/layout/Menu.jsx
@@ -6,21 +6,18 @@ const Menu = props => (
     <aside className="Menu">
         <nav>
             <ul>
-                <li> 
-                    <Link to="/">Home</Link>
+                <li>
+                    <Link to="/">Hooks</Link>
                 </li>
-                <li> 
-                    <Link to="/conteudo01/123">Conteúdo 01</Link>
+                <li>
+                    <Link to="/UseState">useState()</Link>
                 </li>
-                <li> 
-                    <Link to="/conteudo02">Conteúdo 02</Link>
-                </li>                                
-                <li> 
-                    <Link to="/conteudo03">Conteúdo 03</Link>
+                <li>
+                    <Link to="/UseEffect">useEffec()</Link>
                 </li>
-                <li> 
-                    <Link to="/conteudo04">Conteúdo 04</Link>
-                </li>                                
+                <li>
+                    <Link to="/useContext">useContext()</Link>
+                </li>
             </ul>
         </nav>
     </aside>
